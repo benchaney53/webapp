d846b17df6bc68da427f1936279c46bd3c8a9f49
benchaney53 <benchaney53@gmail.com>
2023-03-16 04:38:52 -0500

    2

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
-from flask import Flask
+import os
+from flask import Flask, render_template, request
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileRequired
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'static/uploads'
+app.secret_key = 'secret'
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
+class UploadForm(FlaskForm):
+    photo = FileField(validators=[FileRequired()])
+
+@app.route('/', methods=['GET', 'POST'])
+def upload():
+    form = UploadForm()
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            photo = form.photo.data
+            filename = photo.filename
+            photo.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return 'File uploaded successfully!'
+    return render_template('upload.html', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
